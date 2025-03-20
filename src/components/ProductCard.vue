6ba5d163a024db99f8c692efdf28a6899ab14617
Mid1i <michac.tchernov@yandex.ru>
2025-03-21 01:07:06 +0300

    fix: Немного подкорректированы стили

--- src/components/ProductCard.vue
+++ src/components/ProductCard.vue
@@ -27,12 +27,12 @@
 		--_bg-color: var(--bg-color, #123E66);
 		--_size: var(--size, 200px);
 
-		align-items: center;
 		display: flex;
+		align-items: center;
 		justify-content: center;
-		aspect-ratio: 1;
 		height: var(--_size);
 		width: var(--_size);
+		aspect-ratio: 1;
 
 		background: var(--_bg-color);
 		box-shadow: 10px 10px 5px 0 hsl(from var(--_bg-color) h s calc(l * 0.7));
