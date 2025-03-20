77a020012d1380eb134984e07d634a8f81303382
Mid1i <michac.tchernov@yandex.ru>
2025-03-20 17:23:41 +0300

    feat: Добавлен компонент карточки

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,32 @@
-<script setup lang="ts"></script>
+<script setup lang="ts">
+	import ProductCard from "@/components/ProductCard.vue";
+</script>
 
 
-<template></template>
+<template>
+	<div class="container">
+		<ProductCard 
+			text="Глубина шторма"
+		/>
+		<ProductCard
+			custom-class="new"
+			text="Электро-лазурь"
+		/>
+		<ProductCard
+			custom-class="top"
+			text="Сахарская вата"
+		/>
+	</div>
+</template>
 
 
-<style scoped lang="scss"></style>
+<style lang="scss">
+	.container {
+		align-items: center;
+		display: flex;
+		justify-content: center;
+		gap: 50px;
+		height: 100%;
+		width: 100%;
+	}
+</style>
---
+++ src/components/ProductCard.vue
@@ -0,0 +1,40 @@
+<script setup lang="ts">
+	defineProps<{ 
+		customClass?: string;
+		text: string; 
+	}>();
+</script>	
+
+
+<template>
+	<div :class="['product-card', customClass]">{{ text }}</div>
+</template>
+
+
+<style lang="scss" scoped>
+	.product-card {
+		--_bg-color: var(--bg-color, #123E66);
+		--_padding: var(--padding, 20px);
+
+		align-items: center;
+		display: flex;
+		justify-content: center;
+		aspect-ratio: 1;
+		padding: var(--_padding);
+
+		background: var(--_bg-color);
+		box-shadow: 10px 10px 5px 0 hsl(from var(--_bg-color) h s calc(l * 0.7));
+
+		font-size: 20px;
+		font-weight: 500;
+		color: hsl(from var(--_bg-color) 0 0 calc((50 - l) * 100));
+
+		&.new {
+			--bg-color: #2E9AFF;
+			--padding: 50px;
+		}
+		&.top {
+			--bg-color: #F498AD;
+		}
+	}
+</style>
